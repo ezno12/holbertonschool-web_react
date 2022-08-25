37d2ea2956df37055b1a320a131fab8439991732
tennin12 <bassembenyhia12@gmail.com>
2022-08-25 17:39:07 +0100

    new type and teachclass func added

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -36,3 +36,10 @@ export function executeWork(employee: DirectorInterface | TeacherInterface): str
     if(isDirector(employee)) { return employee.workDirectorTasks() }
     return employee.workTeacherTasks()
 }
+
+type Subjects = "Math" | "History"
+
+export function teachClass(todayClass: Subjects): string {
+    if(todayClass === "Math") { return "Teaching Math" }
+    return "Teaching History"
+}
