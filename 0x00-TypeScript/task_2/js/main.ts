4717ea4421f65096ec9c29f5c418006d7ef28317
tennin12 <bassembenyhia12@gmail.com>
2022-08-25 17:33:24 +0100

    func isdirector and excutework added

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -26,3 +26,13 @@ export function createEmployee(salary: number | string): Director | Teacher {
     if (typeof(salary) === "number" && salary < 500) return new(Teacher)
     return new(Director)
 }
+
+export function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+
+    return 'workDirectorTasks' in employee
+}
+
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+    if(isDirector(employee)) { return employee.workDirectorTasks() }
+    return employee.workTeacherTasks()
+}
