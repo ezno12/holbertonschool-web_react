39190f57f75ac9c5ec995bb4945dc289082c7d80
tennin12 <bassembenyhia12@gmail.com>
2022-08-27 13:46:38 +0100

    task 3 webpack handle css & img added

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,22 @@
+const $ = require("jquery")
+const _ = require("lodash")
+import '../css/main.css'
+
+$("body").append('<div id="logo"></div>')
+$('body').append('<p>Holberton Dashboard</p>')
+$('body').append('<p>Dashboard data for the students</p>')
+$('body').append('<button>Click here to get started</button>')
+$('body').append('<p id="count"></p>')
+$('body').append('<p>Copyright - Holberton School</p>')
+
+
+$("button").on('click', _.debounce(updateCounter, 500))
+
+let count = 0;
+function updateCounter() {
+
+    count = count + 1;
+    $('#count').text(`${count} clicks on the button`)
+}
+
+
