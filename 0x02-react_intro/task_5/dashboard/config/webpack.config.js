081f80542b9011babcc8cc1ed7cec4822e4d1c68
tennin12 <bassembenyhia12@gmail.com>
2022-09-02 16:04:12 +0100

    babel loder and react preset added

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
